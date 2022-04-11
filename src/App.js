b7f94ea2fc8bc6a97d7f057822d37e31369d50d9
StephenFinegan21 <x20191952@student.ncirl.com>
2022-04-11 20:01:46 +0100

    Basic data display functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.scss';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
+  const [countries, setCountries] = useState()
+  const [isLoading, setIsLoading] = useState(true)
+
+  //Will fetch Data from API on first render of this component only 
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all')
       .then(response => {
         console.log(response.data[4].name.common)
         console.log(response.data[4].population)
         console.log(response.data[4].capital[0])
+        setCountries(response.data[0].name.common)
+        setIsLoading(false)
       })
   }, [])
 
 
 
   return (
-    <div className="App">
+    <div className="grid-container">
+      {isLoading ?
+        <p>Loading</p> :
+        <p>{countries}</p> 
+      
+      
+      
+      }
+  
+
+
       
     </div>
   );
