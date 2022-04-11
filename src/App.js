46e38edbd492a7f72a7c43a6fcbafa17c97d6052
StephenFinegan21 <x20191952@student.ncirl.com>
2022-04-11 19:18:10 +0100

    axios fetching data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.scss';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        console.log(response)
+      })
+  }, [])
+
+
+
   return (
     <div className="App">
       
